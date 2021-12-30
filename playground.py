c74d188439ea6c3ca99bf36584b1357145ed429d
brylimz <superuser@brylimz.com>
2021-12-30 09:08:25 +0700

    completed

--- playground.py
+++ playground.py
@@ -1,8 +1,29 @@
 import requests
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+from datetime import datetime
+MY_LAT = -5.397140
+MY_LNG = 105.266792
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+# data = response.json()
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+# iss_position = (longitude, latitude)
+# print(iss_position)
+
+parameter = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameter)
 response.raise_for_status()
 data = response.json()
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-iss_position = (longitude, latitude)
-print(iss_position)
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+print(time_now.hour)
